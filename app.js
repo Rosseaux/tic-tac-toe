c5130eabf159541aa73097932cb35ee08225875b
Ross Hetherton <rosshetherton@live.com>
2022-01-03 13:10:34 +1100

    got the HTML basic version looking nice, scoring on screen, and markers actively working on click for each div. WinCheck function not working properly, not sure why, calling a win before it should

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+
+//Set the array items for the grid, and player trackers
 const grid = { 
         gridspaces: ["grid1", "grid2", "grid3", "grid4", "grid5", "grid6", "grid7", "grid8", "grid9"]
 }
-
 const playerOnePlaces = {
     places: []
 }
-
 const playerTwoPlaces = {
     places: []
 }
-
+//scoring variables
 var playerOneScore = 0;
 var playerTwoScore = 0;
 
+var playerState = "playerOne";
+
+document.getElementById("playerOneScore").innerHTML += playerOneScore;
+document.getElementById("playerTwoScore").innerHTML += playerTwoScore;
+
+//function to be called when a player clicks a square
 function playerMoves(player, placement) {
     if (playerOnePlaces.places.includes(placement) || playerTwoPlaces.places.includes(placement)) {
         console.log("This space is already taken!");
@@ -21,16 +27,17 @@ function playerMoves(player, placement) {
         if (player === "playerOne") {
         playerOnePlaces.places.push(placement);
         winCheck();
-        console.log(playerOnePlaces.places);
+        playerState = "playerTwo";
     }
         if (player === "playerTwo") {
         playerTwoPlaces.places.push(placement);
         winCheck();
-        console.log(playerTwoPlaces.places);
+        playerState = "playerOne";
     }
 }
 }
 
+//function called each time a move is made to see if anyone has won 
 function winCheck() {
     if (playerOnePlaces.places.includes("grid1" && "grid2" && "grid3") ||
     playerOnePlaces.places.includes("grid1" && "grid4" && "grid7") ||
@@ -41,7 +48,7 @@ function winCheck() {
     playerOnePlaces.places.includes("grid1" && "grid5" && "grid9") ||
     playerOnePlaces.places.includes("grid3" && "grid5" && "grid7")) {
         playerOneScore++;
-        return "PLAYER ONE WINS";
+        window.alert("PLAYER ONE WINS");
     }
     if (playerTwoPlaces.places.includes("grid1"&& "grid2" && "grid3") ||
     playerTwoPlaces.places.includes("grid1" && "grid4" && "grid7") ||
@@ -51,8 +58,29 @@ function winCheck() {
     playerTwoPlaces.places.includes("grid7" && "grid8" && "grid9") ||
     playerTwoPlaces.places.includes("grid1" && "grid5" && "grid9") ||
     playerTwoPlaces.places.includes("grid3" && "grid5" && "grid7")) {
-        playerTwoSCore++;
-        return "PLAYER TWO WINS";
+        playerTwoScore++;
+        window.alert("PLAYER TWO WINS");
     }
 }
 
+//grid creation function 
+function setGrid() {
+    let gameBoard = document.getElementById("gameBoard");
+    grid.gridspaces.forEach(function (gridItem) {
+        let boardSpace = document.createElement("div");
+        boardSpace.className = "boardSpace";
+        boardSpace.id = gridItem;
+        gameBoard.appendChild(boardSpace);
+        boardSpace.addEventListener("click", () => {
+            playerMoves(playerState, boardSpace.id);
+            console.log(playerOnePlaces.places);
+            console.log(playerTwoPlaces.places);
+            if (playerState === "playerOne") boardSpace.innerHTML = "X";
+            if (playerState === "playerTwo") boardSpace.innerHTML = "O";
+        })
+        
+    })
+    
+}
+
+setGrid();
